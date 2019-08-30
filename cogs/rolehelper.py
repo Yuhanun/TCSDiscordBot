271204ec1aa1c972a02b27517846759db3b1982d
Yuhanun <yuhanun.c@gmail.com>
2019-08-30 22:24:27 +0200

    Added listener for on_member_join

--- cogs/rolehelper.py
+++ cogs/rolehelper.py
@@ -5,6 +5,7 @@ import traceback
 
 from backend.role_helper import trigger_role, send_error, simple_embed
 
+
 class RoleHelper(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
@@ -16,7 +17,7 @@ class RoleHelper(commands.Cog):
         Triggers role that matches current year into your studies.
         """
         result = await trigger_role(ctx.author, f"Year {year}", ctx.guild)
-        await simple_embed(ctx, ("Removed " if not result else "Added ") + f"role `Year {year}`" )
+        await simple_embed(ctx, ("Removed " if not result else "Added ") + f"role `Year {year}`")
 
     @commands.guild_only()
     @commands.command(name="bachelor")
@@ -25,14 +26,64 @@ class RoleHelper(commands.Cog):
         Triggers bachelor role.
         """
         result = await trigger_role(ctx.author, "Bachelors", ctx.guild)
-        await simple_embed(ctx, ("Removed " if not result else "Added ") + f"role `Bachelors`" )
+        await simple_embed(ctx, ("Removed " if not result else "Added ") + f"role `Bachelors`")
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.NoPrivateMessage):
             return await send_error(ctx, error)
-        
+
         raise error
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member: discord.Member):
+        """
+        Listener for when a member joins, sends member questionaire for nickname etc
+        """
+        _new_member = {
+            "nick": None,
+            "roles": [],
+        }
+
+        def _integer_member_dm(message):
+            def _is_integer():
+                try:
+                    int(message.content)
+                    return True
+                except ValueError:
+                    return False
+
+            return _is_integer() and _check_member_dm(message)
+
+        def _check_member_dm(message):
+            """Checks if message is sent in DM and whether it's sent by the original person that joined"""
+            return message.author.id == member.id and isinstance(message.channel, discord.DMChannel)
+
+        await member.send(f"To gain access to the rest of the channels in the server, you'll have to tell me a few things about yourself.\nPlease tell me your real firstname, for example: Michael")
+        message = await self.bot.wait_for("message", check=_check_member_dm)
+
+        _new_member["nick"] = f"{message.content} | {member.name}"[:31]
+
+        await member.send(f"Perfect!\nAre you a bachelor or a masters student?\n0 for bachelor, 1 for masters.")
+        message = await self.bot.wait_for("message", check=_check_member_dm)
+
+        is_bachelor = not int(message.content)
+        _new_member["roles"].append(
+            discord.utils.get(member.guild.roles,
+                              name="Bachelors" if is_bachelor else "Masters")
+        )
+
+        await member.send(f"Good to know!\nWhich year are you in? If you're in your masters it's either `1` or `2`, if you're in your bachelors then it's 1 through 3.")
+        message = await self.bot.wait_for("message", check=_integer_member_dm)
+
+        _new_member["roles"].append(
+            discord.utils.get(member.guild.roles,
+                              name=f"Year {int(message.content)}")
+        )
+
+        await member.edit(**_new_member)
+        await member.send("Your roles have been applied! Welcome to the server, make sure to read the rules!")
+
 
 def setup(bot):
     bot.add_cog(RoleHelper(bot))
