16510b352e817af3004f6163f17fda2cd789c7d6
Yuhanun Citgez <yuhanun.c@gmail.com>
2019-10-14 15:19:18 +0200

    Added killswitch incase some people can't handle the fun and lwoered chance to 1%

--- cogs/fun.py
+++ cogs/fun.py
@@ -6,12 +6,24 @@ from discord.ext import commands
 
 class Fun(commands.Cog):
     def __init__(self, bot: commands.Bot):
+        self.enabled = True
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        if random.random() < 0.1:
-            await message.add_reaction("tutkegel:633289948711092234")
+        if not self.enabled:
+            return
+        if message.author.bot:
+            return
+        if random.randint(0, 100) == 50:
+            return
+        await message.add_reaction("<:tutkegel:620927895132569601>")
+
+    @commands.command(hidden=True)
+    async def toggle(self, ctx):
+        self.enabled = not self.enabled
+        await ctx.send(f"Set enabled to: {self.enabled}")
+
 
 def setup(bot):
     bot.add_cog(Fun(bot))
