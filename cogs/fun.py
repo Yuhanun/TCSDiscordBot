fac53e43ee4c3f316414a5f09aa770cb3fed5464
Davvos11 <vosdavid2@gmail.com>
2019-10-22 20:51:31 +0200

    Added "Alexa, play despacito" when someone says "this is so sad"

--- cogs/fun.py
+++ cogs/fun.py
@@ -30,6 +30,17 @@ class Fun(commands.Cog):
     async def tutkegel(self, ctx):
         await ctx.send("<:tegel9:634119527680180261><:tegel8:634119528158199841><:tegel7:634119527927513089>\n<:tegel6:634119527868661773><:tegel5:634119527877050399><:tegel4:634119528346812429>\n<:tegel3:634119528825094164><:tegel2:634119528330035200><:tegel1:634119528439218206>")
 
+    # Replies "Alexa, play Despacito" to messages containing "this is so sad"
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        if not self.enabled:
+            return
+        if message.author.bot:
+            return
+        if not "this is so sad" in message.content.lower():
+            return
+        channel = message.channel
+        await channel.send("Alexa, play Despacito")
 
 def setup(bot):
     bot.add_cog(Fun(bot))
