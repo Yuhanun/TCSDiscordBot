b135c1a5c538fda9861e1f826dadaa4578694059
Mark van Wijk <m.g.w.vanwijk@student.utwente.nl>
2019-10-22 13:02:16 +0200

    reformat fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -24,11 +24,20 @@ class Fun(commands.Cog):
         self.enabled = not self.enabled
         await ctx.send(f"Set enabled to: {self.enabled}")
 
-    # Send 3x3 emote grid with tutkegel
-    # emotes are from Davvos11's test discord, so there wont be a need to use up emote space on the TCS discord
+    # Send 3x3 emote grid with tutkegel.
+    # Emotes are from Davvos11's test discord,
+    # therefore, there isn't a need to waste emote space on the TCS discord
     @commands.command(name="tutkegel")
     async def tutkegel(self, ctx):
-        await ctx.send("<:tegel9:634119527680180261><:tegel8:634119528158199841><:tegel7:634119527927513089>\n<:tegel6:634119527868661773><:tegel5:634119527877050399><:tegel4:634119528346812429>\n<:tegel3:634119528825094164><:tegel2:634119528330035200><:tegel1:634119528439218206>")
+        await ctx.send("<:tegel9:634119527680180261>"
+                       "<:tegel8:634119528158199841>"
+                       "<:tegel7:634119527927513089>"
+                       "\n<:tegel6:634119527868661773>"
+                       "<:tegel5:634119527877050399>"
+                       "<:tegel4:634119528346812429>"
+                       "\n<:tegel3:634119528825094164>"
+                       "<:tegel2:634119528330035200>"
+                       "<:tegel1:634119528439218206>")
 
 
 def setup(bot):
