e1d7cdef19555cbfc81f4f5f33663d0412c42988
Davvos11 <vosdavid2@gmail.com>
2019-11-14 17:44:07 +0100

    Added a .avatar command that displays a user's avatar

--- cogs/fun.py
+++ cogs/fun.py
@@ -90,8 +90,19 @@ class Fun(commands.Cog):
                                              url='https://www.vestingbar.nl/en/',
                                              colour=colour)
         embed.set_image(url=img)
-        
+        await ctx.send(embed=embed)
 
+    @commands.command(name="avatar",aliases=['pf'])
+    async def avatar(self, ctx, user: discord.User):
+        """
+        Show a user's discord profile picture/avatar
+        """
+        member = ctx.guild.get_member(user.id)
+        
+        embed: discord.Embed = discord.Embed(title=user.name+"'s avatar:",
+                                             colour=member.colour)
+        embed.set_image(url=user.avatar_url)
+        embed.set_author(name=user.display_name, icon_url=user.avatar_url)
         await ctx.send(embed=embed)
 
 def setup(bot):
