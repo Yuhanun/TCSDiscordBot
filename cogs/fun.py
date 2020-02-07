3dc0798e31bb0e5b437020da99e9d3572cd10b9e
Davvos11 <vosdavid2@gmail.com>
2020-02-08 00:15:54 +0100

    complied to pep8 in meme command

--- cogs/fun.py
+++ cogs/fun.py
@@ -197,9 +197,8 @@ class Fun(commands.Cog):
                 res2 = res2+char+'\n'
         await ctx.send(res+res2)
 
-
     @commands.command(name="meme")
-    async def meme(self,ctx,args):
+    async def meme(self, ctx, args):
         """
         Send a random meme, might take some time for the server to start, takes an argument for the subreddit
         """
@@ -207,7 +206,7 @@ class Fun(commands.Cog):
             session = self.bot._session
             url = 'https://www.reddit.com/r/' + args + '.json?&limit=100'
             try:
-                async with session.get(url,headers={'User-agent': 'oofyeet '}) as resp:
+                async with session.get(url, headers={'User-agent': 'oofyeet '}) as resp:
                     data = await resp.json()
             except aiohttp.ClientConnectorError as e:
                 await ctx.send("Error: "+str(e))
@@ -218,40 +217,41 @@ class Fun(commands.Cog):
                 await ctx.send("/r/"+args+" does not exist")
                 return
 
-            meme = memes[random.randint(0,99)]['data']
-            memeTitle = meme['title']
-            memeURL = meme['url']
-            memeRedditUrl = 'https://reddit.com'+meme['permalink']
-            memeAuthor = meme['author']
-            memeThumbnail = meme['thumbnail']
+            meme = memes[random.randint(0, 99)]['data']
+            meme_title = meme['title']
+            meme_url = meme['url']
+            meme_reddit_url = 'https://reddit.com'+meme['permalink']
+            meme_author = meme['author']
+            meme_thumbnail = meme['thumbnail']
 
             if 'post_hint' in meme:
-                memeType = meme['post_hint']
+                meme_type = meme['post_hint']
             else:
-                memeType = 'text'
+                meme_type = 'text'
 
-            if memeType == 'image':
-                embed: discord.Embed = discord.Embed(colour=0x00ffff,title=memeTitle,url=memeRedditUrl)
-                embed.set_image(url=memeURL)
-            elif memeType == 'text':
-                url = memeRedditUrl+'.json'
+            if meme_type == 'image':
+                embed: discord.Embed = discord.Embed(colour=0x00ffff, title=meme_title, url=meme_reddit_url)
+                embed.set_image(url=meme_url)
+            elif meme_type == 'text':
+                url = meme_reddit_url+'.json'
                 try:
-                    async with session.get(url,headers={'User-agent': 'oofyeet '}) as resp:
+                    async with session.get(url, headers={'User-agent': 'oofyeet '}) as resp:
                         data = await resp.json()
-                    memeTextHTML = data[0]['data']['children'][0]['data']['selftext_html']
-                    memeTextHTML = html.unescape(memeTextHTML)
-                    htmlRoot = PyQuery(memeTextHTML)
-                    memeText = htmlRoot('div.md').html()
-                    memeText = memeText.replace("<p>","").replace("</p>","\n")
-                    if len(memeText) >= 1024:
-                        memeText = memeText[0:1020]+"..."
+                    meme_text_html = data[0]['data']['children'][0]['data']['selftext_html']
+                    meme_text_html = html.unescape(meme_text_html)
+                    html_root = PyQuery(meme_text_html)
+                    meme_text = html_root('div.md').html()
+                    meme_text = meme_text.replace("<p>", "").replace("</p>","\n")
+                    if len(meme_text) >= 1024:
+                        meme_text = meme_text[0:1020]+"..."
                 except aiohttp.ClientConnectorError:
-                    memeText = ""
-                embed: discord.Embed = discord.Embed(description=memeText,colour=0x00ffff,url=memeRedditUrl,title=memeTitle)
+                    meme_text = ""
+                embed: discord.Embed = discord.Embed(description=meme_text, colour=0x00ffff,
+                                                     url=meme_reddit_url, title=meme_title)
             else:
-                embed: discord.Embed = discord.Embed(colour=0x00ffff,url=memeRedditUrl,title=memeTitle)
-                embed.set_thumbnail(url=memeThumbnail)
-            embed.set_author(name="/u/"+memeAuthor, url='https://reddit.com/u/'+memeAuthor)
+                embed: discord.Embed = discord.Embed(colour=0x00ffff, url=meme_reddit_url, title=meme_title)
+                embed.set_thumbnail(url=meme_thumbnail)
+            embed.set_author(name="/u/"+meme_author, url='https://reddit.com/u/' + meme_author)
             await ctx.send(embed=embed)
 
 
