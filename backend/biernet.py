e2b2fb7854abbc44b07dd36cb86fee09b3bca618
Davvos11 <vosdavid2@gmail.com>
2020-02-09 20:01:26 +0100

    biernet command now gets information directly from search page, no need for a second request

--- backend/biernet.py
+++ backend/biernet.py
@@ -19,78 +19,59 @@ async def get_search(self, args):
 async def search(self, search_term):
     search_term = search_term.replace(" ", "-")
     # First try finding crates with the search term as brand name
-    webpage = await get_search(self, {'zoeken': 'true', 'merk': search_term, 'kratten': 'krat-alle'})
+    webpage = await get_search(self, {'zoeken': 'true', 'merk': search_term, 'kratten': 'krat-alle',
+                                      'sorteer': 'prijs-oplopend'})
     url = PyQuery(webpage)('a.merkenUrl').attr('href')
     if url is None:
         # Try finding crates with the search term as search term
-        webpage = await get_search(self, {'zoeken': 'true', 'zoek': search_term, 'kratten': 'krat-alle'})
+        webpage = await get_search(self, {'zoeken': 'true', 'zoek': search_term, 'kratten': 'krat-alle',
+                                          'sorteer': 'prijs-oplopend'})
         url = PyQuery(webpage)('a.merkenUrl').attr('href')
         if url is None:
             # Try finding other offers (not crates) with the search term as brand name
-            webpage = await get_search(self, {'zoeken': 'true', 'merk': search_term})
+            webpage = await get_search(self, {'zoeken': 'true', 'merk': search_term, 'sorteer': 'prijs-oplopend'})
             url = PyQuery(webpage)('a.merkenUrl').attr('href')
             if url is None:
                 # Try finding other offers with the search term as search term
-                webpage = await get_search(self, {'zoeken': 'true', 'zoek': search_term})
+                webpage = await get_search(self, {'zoeken': 'true', 'zoek': search_term, 'sorteer': 'prijs-oplopend'})
                 url = PyQuery(webpage)('a.merkenUrl').attr('href')
                 if url is None:
-                    # If nothing is found, we could always try the search term as result
-                    return search_term
-    # Return only the last part of /bier/merken/[name]
-    return url.split("/")[-1]
+                    # If nothing is found, we throw an exception
+                    raise ValueError(search_term + ' not found, or not on sale')
 
-
-async def get(self, brand):
     host = "https://www.biernet.nl"
-    biernet_url = host + "/bier/merken/" + brand + "/"
-    try:
-        session = self.bot._session
-        # Get the biernet page for the provided beer
-        async with session.get(biernet_url) as resp:
-            webpage = await resp.text()
-    except aiohttp.ClientConnectorError as e:
-        raise e
-    root = PyQuery(webpage)
 
-    title = root('title').html()
-    # Check if the beer could be found
-    if title.startswith("Biermerken") or title.startswith("404"):
-        raise ValueError(brand + ' not found, or not on sale')
-
-    image = root('div#verrander')("img").attr('data-src')
-    # Sometimes we get a webpage with another title than "Biermerken" or "404" but there is no sale information
-    #   (i.e. there is no img (and other data) where we would expect it), if this happens, also return not found
-    if image is None:
-        raise ValueError(brand + ' not found, or not on sale')
-    image = host + image
-
+    first_result = PyQuery(PyQuery(webpage)('li.cardStyle')[0])
     # Get all the various information from the HTML page
-    cheapest_div = root('div#BekijkAlleWinkels')
+    biernet_url = host + first_result('div.item_image')('a').attr('href')
+    image = host + first_result('div.item_image')('a')('img').attr('data-src')
+    brand = first_result('h3.merkenH3')('a')[0].text
 
-    url_element = cheapest_div('a')
-    shop_url = host + url_element.attr('href')
-    shop_image = host + url_element('img').attr('data-src')
+    product = first_result('p.artikel')('a')[0].text
+    product_name = first_result('div.item_image')('a')('img').attr('title')
+    original_price = first_result('p.prijs')('span.van_prijs')[0].text
+    sale_price = first_result('p.prijs')('span.voor_prijs')[0].text
+    sale = PyQuery(first_result('div.informatie')('li.item')[0]).text()
+    sale = sale.replace('korting', 'off')
+    sale_price_liter = PyQuery(first_result('div.informatie')('li.item')[1]).text()
+    end_date = first_result('div.footer-item')('span')[0].text
+    end_date = end_date.replace("t/m ", "").strip()
 
-    shop_name = shop_url.split('--')[-1]
+    biernet_shop_url = host + first_result('div.logo_image')('a').attr('href')
+    shop_name = biernet_shop_url.split('winkel:')[-1]
     shop_name = shop_name.replace('-', ' ').title()
+    shop_image = host + first_result('div.logo_image')('a')('img').attr('data-src')
 
-    text = cheapest_div('p')
+    shop_url = first_result('a.bestelknop').attr('href')
+    if shop_url is None:
+        shop_url = biernet_shop_url
 
-    try:
-        original_price = text('.van_prijs')[0].text
-        sale_price = text('.aanbiedingPrijsPPC')[0].text
-    except IndexError:
-        # Sometimes we find a beer that is not on sale so there is no '.van_prijs' or '.aanbiedingPrijsPPC',
-        # we can then find the sale price using this regex
-        sale_price = re.search("€.*(?= per )", text.text()).group()
-        original_price = " "
-
-    # Encode the URLs that we found, so Discord does not freak out over URLs with spaces, etc.
     biernet_url = urllib.parse.quote(biernet_url, safe=':/%')
     image = urllib.parse.quote(image, safe=':/%')
     shop_url = urllib.parse.quote(shop_url, safe=':/%')
     shop_image = urllib.parse.quote(shop_image, safe=':/%')
 
-    return {'url': biernet_url, 'name': title.split("|")[0], 'img': image,
-            'shop_name': shop_name, 'shop_url': shop_url, 'shop_img': shop_image, 'original_price': original_price,
-            'sale_price': sale_price}
+    return {'url': biernet_url, 'brand': brand, 'name': product_name, 'img': image, 'product': product,
+            'shop_name': shop_name, 'shop_url': shop_url, 'biernet_shop_url': biernet_shop_url, 'shop_img': shop_image,
+            'original_price': original_price, 'sale_price': sale_price, 'sale': sale, 'PPL': sale_price_liter,
+            'end_date': end_date}
